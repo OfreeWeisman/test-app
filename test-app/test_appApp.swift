c91526337e9f11dffbb58c2f1d7da2b2f32c3e29
ofree.weisman <ofree.weisman@ridewithvia.com>
2024-06-20 08:19:28 +0300

    Initial Commit

---
+++ test-app/test_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_appApp.swift
+//  test-app
+//
+//  Created by Ofree Weisman on 20/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct test_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
